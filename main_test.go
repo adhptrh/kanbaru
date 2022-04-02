0eedf415bb64bcca16eeff7f8b41e035d2097643
adhptrh <70842975+adhptrh@users.noreply.github.com>
2022-04-02 21:03:00 +0700

    more test

--- main_test.go
+++ main_test.go
@@ -69,3 +69,54 @@ func TestCheckMatch4(t *testing.T) {
 		t.Error("Wrong, its must be nil")
 	}
 }
+
+func TestCheckMatch5(t *testing.T) {
+	checkPath := "/sample/:wedew"
+	userPath := "/SAMPLE/anjay"
+
+	t.Logf("checkPath : %v", checkPath)
+	t.Logf("userPath : %v", userPath)
+
+	var match = checkMatch(map[string]interface{}{
+		"path":   checkPath,
+		"target": "",
+	}, userPath)
+
+	if match == nil {
+		t.Error("Wrong, its must be not nil")
+	}
+}
+
+func TestCheckMatch6(t *testing.T) {
+	checkPath := "/sample/:wedew/:wedew"
+	userPath := "/SAMPLE/anjay/wtf"
+
+	t.Logf("checkPath : %v", checkPath)
+	t.Logf("userPath : %v", userPath)
+
+	var match = checkMatch(map[string]interface{}{
+		"path":   checkPath,
+		"target": "",
+	}, userPath)
+
+	if match == nil {
+		t.Error("Wrong, its must be not nil")
+	}
+}
+
+func TestCheckMatch7(t *testing.T) {
+	checkPath := "/sample/:wedew/:wedew"
+	userPath := "/SAMPLE/anjay"
+
+	t.Logf("checkPath : %v", checkPath)
+	t.Logf("userPath : %v", userPath)
+
+	var match = checkMatch(map[string]interface{}{
+		"path":   checkPath,
+		"target": "",
+	}, userPath)
+
+	if match != nil {
+		t.Error("Wrong, its must be nil")
+	}
+}
