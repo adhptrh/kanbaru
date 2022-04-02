b0bb62b718b66776ee2693537e5889ee059628b0
adhptrh <70842975+adhptrh@users.noreply.github.com>
2022-04-02 13:42:42 +0700

    pretty cli command

--- cli/main.go
+++ cli/main.go
@@ -1,15 +1,15 @@
 package cli
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/fatih/color"
 )
 
 func Main() {
-	fmt.Println("ok")
 	if os.Args[2] == "add" {
 		if os.Args[3] == "basic-path" {
-
+			color.HiBlue("Creating path " + os.Args[4] + "...")
 			os.Mkdir("path/"+os.Args[4], os.ModeAppend)
 			file, _ := os.Create("path/" + os.Args[4] + "/a.go")
 			file.Write([]byte(`package ` + os.Args[4] + `
@@ -23,6 +23,7 @@ func Main(w http.ResponseWriter, r kanbaru.HttpReq) {
 w.Write([]byte("Basic response"))
 }`))
 			file.Close()
+			color.HiGreen("Successfully created path " + os.Args[4] + "!")
 		}
 	}
 }
